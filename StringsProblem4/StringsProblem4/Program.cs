946a3939fac0a20ae0c0452d5d4dbb149b8eb7d9
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-05-24 19:10:08 +0300

    Solve problem 4 from strings

---
+++ StringsProblem4/StringsProblem4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringsProblem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = ReadFromKeyboard("Text=") ??
+                          ReadFromKeyboard("Text (2nd attempt)=") ??
+                          ReadFromKeyboard("Text (3nd attempt)=") ??
+                          "nu am continut";
+
+            Console.WriteLine(text);
+        }
+
+        static string ReadFromKeyboard(string label)
+        {
+            Console.Write(label);
+            string text = Console.ReadLine();
+
+            return string.IsNullOrWhiteSpace(text)
+                    ? null
+                    : text;
+        }
+    }
+}
